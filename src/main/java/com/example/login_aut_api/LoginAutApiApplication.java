f06c3f5b893b563ef13188d931e12a996353537c
Carlos Felipe de Carvalho <carlosfelipecarvalhosantiago@gmail.com>
2024-09-23 17:40:04 -0300

    Feat: Project creation

---
+++ src/main/java/com/example/login_aut_api/LoginAutApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_aut_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAutApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_aut_api/LoginAutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_aut_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
